134558e4f0b05e8d53ec0159d3fac0797f0b338e
nippun <nippuntyagi@gmail.com>
2017-02-11 11:00:11 +0530

    updated zipboard experiencs

---
+++ phpform.php
@@ -0,0 +1,33 @@
+<?php
+// Fetching Values from URL.
+$name = $_POST['name1'];
+$email = $_POST['email1'];
+$message = $_POST['message1'];
+$email = filter_var($email, FILTER_SANITIZE_EMAIL); // Sanitizing E-mail.
+// After sanitization Validation is performed
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+$subject = "Message Recieved Will Contact Soon";
+// To send HTML mail, the Content-type header must be set.
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'From: contact@nippuntyagi.in' . "\r\n";      // Sender's Email
+//$headers .= 'Reply-To: contact@nippuntyagi.in' . "\r\n"; 
+$headers .= 'Bcc: contact@nippuntyagi.in' . "\r\n";      //Carbon copy to Sender
+$template = '<div style="padding:50px; color:white;">Hello ' . $name . ',<br/>'
+. '<br/>Thank you...! For Contacting Us.<br/><br/>'
+. 'Name:' . $name . '<br/>'
+. 'Email:' . $email . '<br/>'
+. 'Message:' . $message . '<br/><br/>'
+. 'This is a Contact Confirmation mail.'
+. '<br/>'
+. 'We Will contact You as soon as possible .</div>';
+$sendmessage = "<div style=\"background-color:#7E7E7E; color:white;\">" . $template . "</div>";
+// Message lines should not exceed 70 characters (PHP rule), so wrap it.
+$sendmessage = wordwrap($sendmessage, 70);
+// Send mail by PHP Mail Function.
+mail($email, $subject, $sendmessage, $headers);
+echo "Your Query has been received, We will contact you soon.";
+} else {
+echo "not a valid email";
+}
+?>
